aed48ff58ff2432772d526ccbcb3d6cdb30f1045
Denis <medvedevdenis118@gmail.com>
2023-08-11 08:57:05 +0300

    homework 5

--- 4/advertisements/advertisements/settings.py
+++ 4/advertisements/advertisements/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'advertisements.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [Path.joinpath(BASE_DIR, "templates")],
+        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
