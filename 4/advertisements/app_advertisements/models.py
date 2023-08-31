5fc59903e60b0fa94857a30043f9ed13b38b8b3f
Denis <medvedevdenis118@gmail.com>
2023-08-31 09:25:17 +0300

    homework 8

--- 4/advertisements/app_advertisements/models.py
+++ 4/advertisements/app_advertisements/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone, html
 
 # Create your models here.
 class Advertisement(models.Model):
@@ -14,3 +16,22 @@ class Advertisement(models.Model):
 
     class Meta:
         db_table = "advertisements"
+
+    @admin.display(description="Дата созданий")
+    def created_date(self):
+        print(timezone.now().date())
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H-%M-%S")
+            return html.format_html(
+                "<span style='color: green; font-weight: bold;' >Сегодня в {}</span>", created_time
+            )
+        return self.created_at.strftime("%d.%m.%y в %H:%M:%S")
+    
+    @admin.display(description="Дата обновдений")
+    def updated_date(self):
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime("%H-%M-%S")
+            return html.format_html(
+                "<span style='color: yellow'; font-weight: bold; '>Сегодня в {}</span>", updated_time
+            )
+        return self.updated_at.strftime("%d.%m.%y в %H:%M:%S")
